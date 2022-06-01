d91f0b29e438108764566edce616a7bd9d7543ad
Jastrzab <kubixxu@gmail.com>
2022-06-01 19:57:37 +0200

    Added esc menu and player movement

---
+++ Kocury z Wólki Małej/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+using StarterAssets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isGamePaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject playerObject;
+    public GameObject watergunObject;
+    public GameObject gunPointer;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        gunPointer.SetActive(true);
+        Time.timeScale = 1.0f;
+        isGamePaused = false;
+        playerObject.GetComponent<FirstPersonController>().enabled = true;
+        watergunObject.GetComponent<RaycastGunShot>().enabled = true;
+        Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.;
+    }
+
+    private void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        gunPointer.SetActive(false);
+        Time.timeScale = 0.0f;
+        isGamePaused = true;
+        playerObject.GetComponent<FirstPersonController>().enabled = false;
+        watergunObject.GetComponent<RaycastGunShot>().enabled = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
---
+++ Kocury z Wólki Małej/Assets/Scripts/RaycastGunShot.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class RaycastGunShot : MonoBehaviour
+{
+    public Camera playerCamera;
+    public Transform streamOrigin;
+    public float gunRange = 50f;
+    public float streamDuration = 0.05f;
+    public float fireRate = 0.2f;
+
+    LineRenderer streamLine;
+    float fireTimer;
+    private AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();  
+    }
+    void Awake()
+    {
+        streamLine = GetComponent<LineRenderer>(); 
+    }
+
+    void Update()
+    {
+        fireTimer += Time.deltaTime;
+       if(Input.GetButtonDown("Fire1") && fireTimer > fireRate)
+        {
+            fireTimer = 0;
+            streamLine.SetPosition(0, streamOrigin.position);
+            Vector3 rayOrigin = playerCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+            RaycastHit hit;
+            if(Physics.Raycast(rayOrigin, playerCamera.transform.forward, out hit, gunRange))
+            {
+                streamLine.SetPosition(1, hit.point);
+                if(hit.transform.gameObject.tag == "Mouse")
+                {
+                    //Debug.Log("Enemy hit!");
+                    
+                }
+            } 
+            else
+            {
+                streamLine.SetPosition(1, rayOrigin + (playerCamera.transform.forward * gunRange));
+            }
+            audioSource.PlayOneShot(audioSource.clip);
+            StartCoroutine(ShootLaser());
+        } 
+    }
+
+    IEnumerator ShootLaser()
+    {
+        streamLine.enabled = true;
+        yield return new WaitForSeconds(streamDuration);
+        streamLine.enabled = false;
+    }
+}
